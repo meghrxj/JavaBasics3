ec67bbd6bdce1149eae755ac63d21f890cd55f0a
meghrxj <hello.meghrajjj@gmail.com>
2021-10-15 20:30:17 +0530

    h

--- HashMap.java
+++ HashMap.java
@@ -1,3 +1,20 @@
+import java.util.HashMap;
 public class HashMap {
-    
+
+    public static void main(String[] args) {
+
+    int a = 10;
+    String b = "hello";
+    int[] c = {1, 2, 3};
+    //what if we have millions variables?
+
+        //HASHMap
+        HashMap happy = new HashMap();
+
+            happy.put("a", 10);
+            happy.put("b", 88);
+            happy.put("c", 3);;
+
+
+    }
 }
