863bb8af1980f3b9798de5de1c4dcb0870c27462
meghrxj <hello.meghrajjj@gmail.com>
2021-10-15 22:02:18 +0530

    Create StacknStuff.java

---
+++ StacknStuff.java
@@ -0,0 +1,12 @@
+import java.util.Stack;
+
+public class StacknStuff {
+    public static void main(String[] args){
+
+        Stack<Character> tower = new Stack<Character>();
+
+        tower.add('R');
+        tower.add('B');
+        tower.add('Y'); //will be top of the stack
+    }
+}
