56abeee3af991ac1888597f00abee8a0d447ce3d
meghrxj <hello.meghrajjj@gmail.com>
2021-10-15 21:03:56 +0530

    update

--- HashSet1.java
+++ HashSet1.java
@@ -1,4 +1,21 @@
+import java.util.HashSet;
+
 public class HashSet1 {
-    
-    
+
+    public static void main(String[] args){
+
+
+        HashSet<String> h = new HashSet<String>();
+        h.add("Lemur");
+        h.add("orangatang");
+        h.add("spider monkey");
+        h.add("Gorilla");
+
+        h.remove("Gorilla");
+        //h.clear(); 
+        System.out.println(h.contains("spider monkey"));
+        System.out.println(h);
+
+
+    }
 }
