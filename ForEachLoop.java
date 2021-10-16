2ac518ef4b819ad990464d991ed1d9b9db4162cb
meghrxj <hello.meghrajjj@gmail.com>
2021-10-16 22:47:47 +0530

    Update ForEachLoop.java

--- ForEachLoop.java
+++ ForEachLoop.java
@@ -1,7 +1,29 @@
 public class ForEachLoop {
     public static void main(String[] args){
 
-        
+        //regular for loop
+
+        /*for(int i = 0; i < 5; i++) {
+            System.out.println(i);
+        } */
+
+        //for each loop
+
+        String[] cars = {"BMW M2", "Veloster N", "GTI" };
+
+    
+        for(String car : cars) {
+            System.out.println(car);
+        }
+
+
+
+
+
+
+
+
+
 
     }
 }
